dff2ea9d022b4317a720819d314cd23dfb18e4e2
edena <djaziz12393@gmail.com>
2021-07-06 09:38:38 +0300

    Add project files.

---
+++ GitFun.VS2019.Create/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitFun.VS2019.Create
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
