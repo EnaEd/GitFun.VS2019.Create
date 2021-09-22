391c3ac2cb70500215523d66c39c02f55dd5c329
ed <djaziz12393@gmail.com>
2021-09-22 12:32:21 +0300

    right commit

--- GitFun.VS2019.Create/Program.cs
+++ GitFun.VS2019.Create/Program.cs
@@ -6,7 +6,7 @@ namespace GitFun.VS2019.Create
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
